48f8ac4c0eb6b3b0d0f8b0dd7ec6be8ec1342061
Alessandro Losavio <alessandro.losavio@mail.polimi.it>
2019-03-16 00:35:00 +0100

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Alessandro Losavio on 16/03/2019.
+//  Copyright © 2019 Losavio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
